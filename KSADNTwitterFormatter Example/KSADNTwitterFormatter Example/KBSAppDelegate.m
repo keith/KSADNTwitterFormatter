83524368e8cfe576e4f73a010a23dc5deddd6363
Keith Smiley <keithbsmiley@gmail.com>
2013-03-30 13:36:09 -0400

    Short example log statement

--- KSADNTwitterFormatter Example/KSADNTwitterFormatter Example/KBSAppDelegate.m
+++ KSADNTwitterFormatter Example/KSADNTwitterFormatter Example/KBSAppDelegate.m
@@ -7,12 +7,14 @@
 //
 
 #import "KBSAppDelegate.h"
+#import "KSADNTwitterFormatter.h"
 
 @implementation KBSAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    NSString *postText = @"This is a long string that is much to long for a Twitter post but isn't too long for an ADN post. Luckily this library will truncate it and add the URL passed (typically the ADN URL) to the end.";
+    NSLog(@"\n\nOriginal Post: %@\n\nTwitter Post: %@", postText, [KSADNTwitterFormatter formatTwitterStringWithString:postText andURL:[NSURL URLWithString:@"http://thelinktosomepostid.com/"]]);
 }
 
 @end
